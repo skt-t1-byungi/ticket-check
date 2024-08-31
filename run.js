544705807cfda3dd9f72edf8da861908a348ec17
byungi <zeronin2@gmail.com>
2024-08-31 12:09:20 +0900

    히게단내한

--- run.js
+++ run.js
@@ -2,8 +2,10 @@ const { fetch } = require('undici')
 
 main()
 
+const TICKET_NAME = '히게단내한'
+
 async function main() {
-    const resp = await fetch('https://api-ticketfront.interpark.com/v1/goods/23001998/playSeq/PlaySeq/001/REMAINSEAT')
+    const resp = await fetch('https://api-ticketfront.interpark.com/v1/goods/24011642/playSeq/PlaySeq/001/REMAINSEAT')
     if (!resp.ok) {
         console.log(`HTTP error! status: ${resp.status}`)
         return
@@ -11,15 +13,15 @@ async function main() {
     const data = await resp.json()
     const seat = data.data.remainSeat.find(seat => seat.remainCnt > 0)
     if (!seat) {
-        console.log('텔즈위버 표가 없습니다.')
+        console.log(`${TICKET_NAME} 표가 없습니다.`)
         return
     }
     await fetch(process.env.SLACK_WEBHOOK_URL, {
         method: 'POST',
         body: JSON.stringify({
-            username: '텔즈위버남은표',
+            username: `${TICKET_NAME}남은표`,
             icon_emoji: ':ticket:',
-            text: `텔즈위버 남은 표가 있습니다! (${seat.seatGradeName}: ${seat.remainCnt}개) \n https://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GoodsCode=23001998`,
+            text: `${TICKET_NAME} 남은 표가 있습니다! (${seat.seatGradeName}: ${seat.remainCnt}개) \n https://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GoodsCode=23001998`,
         }),
     })
 }
