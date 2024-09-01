23f478a857169e2526cb3956d6b9cec0ffd4e90a
byungi <zeronin2@gmail.com>
2024-09-01 19:33:38 +0900

    fix

--- run.js
+++ run.js
@@ -3,14 +3,15 @@ const { fetch } = require('undici')
 main()
 
 const TICKET_NAME = '히게단내한'
+const TICKET_ID = '24011642'
 
 async function main() {
     const resp = await fetch(
-        'https://thingproxy.freeboard.io/fetch/https://api-ticketfront.interpark.com/v1/goods/24011642/playSeq/PlaySeq/001/REMAINSEAT'
+        `https://thingproxy.freeboard.io/fetch/https://api-ticketfront.interpark.com/v1/goods/${TICKET_ID}/playSeq/PlaySeq/001/REMAINSEAT`
     )
     if (!resp.ok) {
         console.log(`HTTP error! status: ${resp.status}`)
-        return
+        process.exit(1)
     }
     const data = await resp.json()
     const seat = data.data.remainSeat.find(seat => seat.remainCnt > 0)
@@ -23,7 +24,7 @@ async function main() {
         body: JSON.stringify({
             username: `${TICKET_NAME}남은표`,
             icon_emoji: ':ticket:',
-            text: `${TICKET_NAME} 남은 표가 있습니다! (${seat.seatGradeName}: ${seat.remainCnt}개) \n https://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GoodsCode=23001998`,
+            text: `${TICKET_NAME} 남은 표가 있습니다! (${seat.seatGradeName}: ${seat.remainCnt}개) \n https://tickets.interpark.com/goods/${TICKET_ID}`,
         }),
     })
 }
